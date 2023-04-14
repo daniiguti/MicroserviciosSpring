0cc375448b38cb1c32d8811f9f1c761fd321f7f2
Dani <danielgutierrezbaena@gmail.com>
2023-04-14 09:15:21 +0200

    fallo sintaćtico en pom.xml

---
+++ ms_registry-service/src/main/java/com/example/demo/MsRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRegistryServiceApplication.class, args);
+	}
+
+}
